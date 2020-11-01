6b9e2607f6887ce633496dffcfaa4c49ed6f1139
blackSmith <ibrahim21haddad872@gmail.com>
2020-11-01 11:19:11 +0000

    Script And Style

---
+++ script.js
@@ -0,0 +1,5 @@
+let btn = document.querySelector(".btnMenu");
+let menubar = document.querySelector(".items");
+btn.addEventListener("click",function(){
+      menubar.classList.toggle("baritems")
+})
